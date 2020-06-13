0c104ae995955b6203a0f7620c4b14fe0412848c
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-13 15:45:49 -0400

    check guess function in letter.js

--- letter.js
+++ letter.js
@@ -1,7 +1,21 @@
 //constructor function that takes in a letter
 function Letter(letter) {
-    //a string value that stores the underlying character for the letter
-    this.letter = letter;
-    //a boolean value that stores whether that letter has been guessed yet
-    this.guessed = false;
+  //a string value that stores the underlying character for the letter
+  this.letter = letter;
+  //a boolean value that stores whether that letter has been guessed yet
+  this.guessed = false;
+
+  //function that returns the underlying character if the letter has been guessed, or a placeholder(like an underscore) if the letter has not been guessed
+  // use toString - js will call that function automatically whenever casting that object to a string
+  this.toString = function () {
+    //user guessed incorrectly letter return placeholder (underscore)
+    if (this.guessed === false) {
+      return "_";
+    } else {
+      //user guessed correctly return the letter they guessed
+      return this.letter;
+    }
+  };
+
+  //function that takes a character as an argument and checks it agains the underlying character, updating the stored boolean value to true if it was guessed correctly
 }
