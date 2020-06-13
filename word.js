6c92e761728cffce8376e3680684121aab016988
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-13 16:02:54 -0400

    require letter.js into word.js and comment out steps to be taken

---
+++ word.js
@@ -0,0 +1,8 @@
+//import letter.js file
+var Letter = require("./letter.js");
+
+//an array of new Letter objects representing the letters of underlying word
+
+//a func that returns a string representing the word. This should call the function on each letter object (the first function defined in letter.js) that displays the character or an underscore and concatenate those together.
+
+//a func that takes a character as an argument and call the guess func on each letter object(the second funtion defined in letter.js)
