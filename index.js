c4dc08df1a5a1885e323883c9fd0e4af09ee6cc5
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-14 10:43:26 -0400

    game is functioning

--- index.js
+++ index.js
@@ -18,108 +18,105 @@ let wordBank = [
   "mulan",
 ];
 
-let wordToGuess;
-let wordToGuessObj;
-let NumOfGuessed;
+//Global variables
+let guesses;
+let pickedWords;
+let word;
+let pickedWord;
 
-function randomWord() {
-  wordToGuess = words[Math.floor(Math.random() * wordBank.length)];
+//Initial prompt for user to play game
+function prompt() {
+  pickedWords = [];
+  console.log("Hello, and welcome to Word Guess with Disney Princesses!");
+  console.log("------------------------------------------");
+//function for game to start
+  playGame();
 }
 
-function PromptCharGuessing() {
-  inquirer
-    .prompt([
-      {
-        name: "letter",
-        message: "Guess a letter :",
-        validate: function (value) {
-          if (value.length === 1) {
-            return true;
-          }
-          return false;
-        },
-      },
-    ])
-    .then(function (answer) {
-      let correct = false;
-      correct.wordToGuessObj.check(answer.letter.toLowerCase());
-      console.log(wordToGuessObj.wordprep());
-
-      if (correct) {
-        console.log("Correct!");
-        NumOfGuessed++;
-      } else {
-        console.log("Incorrect!");
-        NumOfGuessess--;
-        console.log(
-          "--------------" + NumOfGuessess + "guessess left------------"
-        );
-      }
-      if (NumOfGuessess === 0) {
-        GameOver();
-      } else if (NumOfGuessed === wordToGuess.length) {
-        console.log("You got it right!");
-        PlayAgain();
-      } else {
-        PromptCharGuessing();
-      }
-    });
-}
-
-function PlayGame() {
-  NumOfGuessess = 9;
-  NumOfGuessess = 0;
-  console.log(
-    "----------------------------New Game--------------------------------"
-  );
-  console.log("------------" + NumOfGuessess + " guesses remaining-----------");
-  generateword();
-  wordToGuessObj = new Word.word(wordToGuess);
-  PromptCharGuessing();
-}
-
-function PlayAgain() {
-  inquirer
-    .prompt([
-      {
-        name: "confirm",
-        type: "confirm",
-        message: "Would you like to play again?",
-      },
-    ])
-    .then(function (answer) {
-      if (answer.confirm === true) {
-        PlayGame();
-      } else {
-        console.log("Goodbye");
-        process.exit();
-      }
-    });
-}
-
-function GameOver() {
-  console.log("----------------------GAME OVER------------------------------");
-
-  inquirer.prompt([
-    {
-      name: "confirm",
-      type: "confirm",
-      message: "Would you like to play again?",
-    },
-  ]).then(function (answer) {
-      if (answer === true) {
-          PlayGame();
-      } else {
-          console.log("Goodbye");
-          process.exit();
-      }
-  });
+function playGame() {
+  pickedWord = "";
+  guesses = 15;
+  if (pickedWords.length < wordBank.length) {
+    pickedWord = getWord();
+  } else {
+    // WIN CONDITION
+    console.log("You know your Disney Princesses, Congratulations!");
+    continuePrompt();
+  }
+  if (pickedWord) {
+    word = new Word(pickedWord);
+    word.displayWord();
+    makeGuess();
+  }
 }
 
 //Randomly selects a word and uses the Word constructor to store it
-//let randomIdx = Math.floor(Math.random() * wordBank.length);
-//let randomWord = new Word(wordBank[randomIdx]);
-//console.log(randomWord.displayWord());
-//console.log(new Word(randomWord))
+function getWord() {
+  let rand = Math.floor(Math.random() * wordBank.length);
+  let randomWord = wordBank[rand];
+  if (pickedWords.indexOf(randomWord) === -1) {
+    pickedWords.push(randomWord);
+    return randomWord;
+  } else {
+    return getWord();
+  }
+}
+
+function makeGuess() {
+  let checker = [];
+  inquirer
+    .prompt([
+      {
+        name: "guessedLetter",
+        message:
+          word.displayWord() +
+          "\nGuess a letter!" +
+          "\nGuesses Left: " +
+          guesses,
+      },
+    ])
+    .then((data) => {
+      word.wordArray.forEach((letter) => {
+        letter.letterCheck(data.guessedLetter);
+        checker.push(letter.toString());
+      });
+      if (guesses > 0 && checker.indexOf("_") !== -1) {
+        guesses--;
+        if (guesses === 0) {
+          console.log("YOU RAN OUT OF GUESSES! GAME OVER.");
+          continuePrompt();
+        } else {
+          makeGuess();
+        }
+      } else {
+        console.log("CONGRATULATIONS! YOU GOT THE WORD!");
+        console.log(word.displayWord());
+        playGame();
+      }
+    });
+}
+
+function continuePrompt() {
+  inquirer
+    .prompt([
+      {
+        name: "continue",
+        type: "list",
+        message: "Would you like to play again?",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((data) => {
+      if (data.continue === "Yes") {
+        prompt();
+      } else {
+        console.log("Thanks for playing!");
+      }
+    });
+}
+
+prompt();
+
+
 
 //Prompts the user for each guess and keeps track of the user's remaining guesses
